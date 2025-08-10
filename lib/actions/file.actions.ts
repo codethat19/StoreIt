abde980d2d8c3e2e573be6f3f14a4cffcfb2d327
codethat19 <aayushrastogi1997@gmail.com>
2025-08-10 21:06:53 +0530

    add functions to support sort, search and arrange file types into their respective section

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -78,17 +78,17 @@ const createQueries = (
 		]),
 	];
 
-	// if (types.length > 0) queries.push(Query.equal("type", types));
-	// if (searchText) queries.push(Query.contains("name", searchText));
-	// if (limit) queries.push(Query.limit(limit));
+	if (types.length > 0) queries.push(Query.equal("type", types));
+	if (searchText) queries.push(Query.contains("name", searchText));
+	if (limit) queries.push(Query.limit(limit));
 
-	// if (sort) {
-	// 	const [sortBy, orderBy] = sort.split("-");
+	if (sort) {
+		const [sortBy, orderBy] = sort.split("-");
 
-	// 	queries.push(
-	// 		orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
-	// 	);
-	// }
+		queries.push(
+			orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+		);
+	}
 
 	return queries;
 };
